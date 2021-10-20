9744c81c7b78d303aa1a88c700baa642669ea92e
MlleKan <Kan.dawson@protonmail.com>
2021-10-20 13:41:44 -0400

    Added images

--- index.js
+++ index.js
@@ -7,3 +7,11 @@ function mDown(obj) {
     obj.style.backgroundColor="#D94A38";
     obj.innerHTML="Thank You";
   }
+
+  const button = document.querySelector('button');
+
+  button.addEventListener('click', event => {
+    button.textContent = `Click count: ${event.detail}`;
+  });
+  
+  
