611205ead0d2b607d391ff3c88d857098d6cd117
MlleKan <Kan.dawson@protonmail.com>
2021-10-24 23:40:37 -0400

    Updated JS

--- index.js
+++ index.js
@@ -20,4 +20,8 @@ document.getElementById.innerHTML = alert ("Hi "+Name+" ,Nice To Meet You")
 
 }
 
+let age = prompt('PLEASE VERIFY YOUR AGE')
+if (age < 10 ){
+  document.getElementById("content").innerHTML = "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/XOnHtStmbCI?start=10\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>"
 
+}
