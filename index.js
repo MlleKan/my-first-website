55a8f0efee8ad0c9677b45a577bed9c1ee2760bb
MlleKan <Kan.dawson@protonmail.com>
2021-10-25 10:13:38 -0400

    Updated JS

--- index.js
+++ index.js
@@ -16,12 +16,6 @@ alert("HI THERE, THANK YOU FOR VISITING MY WEBSITE")
 {
 let Name = prompt("Hello Please Enter Your Name")
 console.log("Hi "+Name+ " ,Nice To Meet You")
-document.getElementById.innerHTML = alert ("Hi "+Name+" ,Nice To Meet You")
-
-}
-
-let age = prompt('PLEASE VERIFY YOUR AGE')
-if (age < 10 ){
-  document.getElementById("content").innerHTML = "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/XOnHtStmbCI?start=10\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>"
+document.getElementById.innerHTML = prompt ("Hi "+Name+" ,Nice To Meet You")
 
 }
